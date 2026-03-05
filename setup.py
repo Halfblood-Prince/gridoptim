e3b2bbf129c48eb9ded3b4a09cdf0e43edd450e5
Halfblood-Prince <147537283+Halfblood-Prince@users.noreply.github.com>
2026-03-05 23:23:46 +0100

    Refactor setup.py to use Extension for C++ module

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
-from setuptools import setup
-from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup, Extension
+from pybind11 import get_include
+from pybind11.setup_helpers import build_ext
 
 ext_modules = [
-    Pybind11Extension(
+    Extension(
         "gridoptim._core",
-        sources=["cpp/gridoptim_core.cpp", "cpp/tinyexpr.c"],
-        include_dirs=["cpp"],
-        cxx_std=17,
-        # OpenMP: best effort. Works on many setups; if it fails on some platforms,
-        # we can refine flags per-compiler.
-        extra_compile_args=[],
-        extra_link_args=[],
+        sources=[
+            "cpp/gridoptim_core.cpp",  # C++
+            "cpp/tinyexpr.c",          # C
+        ],
+        include_dirs=[
+            "cpp",
+            get_include(),
+        ],
+        language="c++",
+        extra_compile_args=["-std=c++17"],
     )
 ]
 
