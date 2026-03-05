5ec3a1cd6ea7b7ba1a7f31b475feb117177e34cc
Halfblood-Prince <147537283+Halfblood-Prince@users.noreply.github.com>
2026-03-05 23:39:43 +0100

    Refactor setup.py to simplify extension configuration

--- setup.py
+++ setup.py
@@ -2,23 +2,12 @@ from setuptools import setup, Extension
 from pybind11 import get_include
 from pybind11.setup_helpers import build_ext
 
-# Limited API for Python 3.10+ => 0x030A0000
-PY_LIMITED = "0x030A0000"
-
 ext_modules = [
     Extension(
         "gridoptim._core",
-        sources=[
-            "cpp/gridoptim_core.cpp",
-            "cpp/tinyexpr.c",
-        ],
-        include_dirs=[
-            "cpp",
-            get_include(),
-        ],
+        sources=["cpp/gridoptim_core.cpp", "cpp/tinyexpr.c"],
+        include_dirs=["cpp", get_include()],
         language="c++",
-        define_macros=[("Py_LIMITED_API", PY_LIMITED)],
-        py_limited_api=True,
         extra_compile_args=["-std=c++17"],
     )
 ]
