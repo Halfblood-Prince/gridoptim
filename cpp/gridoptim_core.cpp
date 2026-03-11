b718c88a2a2ce29fd2bdb7696de10561f67eec64
Halfblood-Prince <147537283+Halfblood-Prince@users.noreply.github.com>
2026-03-11 12:36:56 +0100

    Update gridoptim_core.cpp

--- cpp/gridoptim_core.cpp
+++ cpp/gridoptim_core.cpp
@@ -1,6 +1,6 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include <tinyexpr.h>
+#include "tinyexpr.h"
 #include <vector>
 #include <string>
 #include <limits>
@@ -34,7 +34,9 @@ py::dict optimise(
 
     std::vector<double> global_point(dim);
 
+#ifdef _OPENMP
     #pragma omp parallel
+#endif
     {
         std::vector<double> vars(dim);
         std::vector<te_variable> te_vars(dim);
@@ -43,16 +45,26 @@ py::dict optimise(
             te_vars[i] = { names[i].c_str(), &vars[i] };
         }
 
-        int err;
-        te_expr* compiled = te_compile(expr.c_str(), te_vars.data(), dim, &err);
+        int err = 0;
+        te_expr* compiled = te_compile(expr.c_str(), te_vars.data(), (int)dim, &err);
+
+        if (!compiled) {
+#ifdef _OPENMP
+            #pragma omp critical
+#endif
+            {
+                throw std::runtime_error("Failed to compile expression with tinyexpr");
+            }
+        }
 
         double best = maximise ? -std::numeric_limits<double>::infinity()
                                :  std::numeric_limits<double>::infinity();
         std::vector<double> best_point(dim);
 
+#ifdef _OPENMP
         #pragma omp for
+#endif
         for (int64_t idx = 0; idx < total; idx++) {
-
             int64_t t = idx;
 
             for (size_t d = 0; d < dim; d++) {
@@ -69,11 +81,12 @@ py::dict optimise(
             }
         }
 
+#ifdef _OPENMP
         #pragma omp critical
+#endif
         {
             if ((maximise && best > global_best) ||
                 (!maximise && best < global_best)) {
-
                 global_best = best;
                 global_point = best_point;
             }
@@ -86,11 +99,15 @@ py::dict optimise(
     result["value"] = global_best;
 
     py::dict point;
-
-    for (size_t i = 0; i < dim; i++)
+    for (size_t i = 0; i < dim; i++) {
         point[names[i].c_str()] = global_point[i];
+    }
 
     result["point"] = point;
-
     return result;
+}
+
+PYBIND11_MODULE(_core, m) {
+    m.doc() = "gridoptim core";
+    m.def("optimise", &optimise, "Run grid search optimisation");
 }
