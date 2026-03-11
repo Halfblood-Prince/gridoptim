e1246f9ef36e83371b7da280c79bdacc37485dd6
Halfblood-Prince <147537283+Halfblood-Prince@users.noreply.github.com>
2026-03-11 12:37:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,26 +2,18 @@ from setuptools import setup, Extension
 import pybind11
 import sys
 
-compile_args = ["-O3"]
-link_args = []
-
 if sys.platform == "win32":
-    compile_args.append("/openmp")
+    compile_args = ["/O2", "/openmp"]
+    link_args = []
 else:
-    compile_args.append("-fopenmp")
-    link_args.append("-fopenmp")
+    compile_args = ["-O3", "-fopenmp"]
+    link_args = ["-fopenmp"]
 
 ext_modules = [
     Extension(
         "gridoptim._core",
-        [
-            "cpp/gridoptim_core.cpp",
-            "cpp/tinyexpr.c",
-        ],
-        include_dirs=[
-            pybind11.get_include(),
-            "cpp",
-        ],
+        ["cpp/gridoptim_core.cpp", "cpp/tinyexpr.c"],
+        include_dirs=[pybind11.get_include(), "cpp"],
         language="c++",
         extra_compile_args=compile_args,
         extra_link_args=link_args,
